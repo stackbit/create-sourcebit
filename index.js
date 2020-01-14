3c914b45fb8e4a262e5998d6b5a3e29a3d3400ad
Eduardo Bouças <mail@eduardoboucas.com>
2020-01-14 14:12:20 +0000

    Add env files

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const Wizard = require("./lib/wizard");
 
 (async () => {
   const wizard = new Wizard();
-  const plugins = await wizard.start();
-  const configPath = path.join(process.cwd(), "sourcebit.js");
+  const { plugins, envFileOptions } = await wizard.start();
   const moduleExports = util.inspect(
     {
       plugins
@@ -17,12 +16,35 @@ const Wizard = require("./lib/wizard");
   );
   const config = `module.exports = ${moduleExports}\n`;
 
+  // Writing configuration file.
   try {
+    const configPath = path.join(process.cwd(), "sourcebit.js");
+
     fs.writeFileSync(configPath, config);
 
     console.log(`\nConfiguration saved to ${chalk.bold(configPath)}.`);
   } catch (error) {
     console.log("ERROR: Could not create configuration file.");
+
+    process.exit(1);
+  }
+
+  // Writing env file.
+  try {
+    const envFileData = Object.keys(envFileOptions)
+      .map(key => `${key}=${envFileOptions[key]}`)
+      .join("\n");
+
+    if (envFileData.length > 0) {
+      const envFilePath = path.join(process.cwd(), ".env");
+
+      fs.writeFileSync(envFilePath, envFileData + "\n");
+
+      console.log(`\Environment file saved to ${chalk.bold(envFilePath)}.`);
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("ERROR: Could not create environment file.");
 
     process.exit(1);
   }
